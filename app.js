31b2663b399b82d759717940a338d04adb2058c8
xaviguasch <xaviguasch@gmail.com>
2019-02-26 12:19:51 +0100

    saved the default items to the database

--- app.js
+++ app.js
@@ -38,6 +38,14 @@ const item3 = new Item({
 
 const defaultItems = [item1, item2, item3]
 
+Item.insertMany(defaultItems, function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Successfully saved the default items to the collection');
+    }
+})
+
 
 
 app.get("/", function (req, res) {
