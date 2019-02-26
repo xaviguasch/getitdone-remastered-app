c2251b985b6a13b3362483bce1a91f3eea0daf02
xaviguasch <xaviguasch@gmail.com>
2019-02-26 13:20:31 +0100

    solved bug, now the items don't repeat themselves when restarting the server

--- app.js
+++ app.js
@@ -38,23 +38,31 @@ const item3 = new Item({
 
 const defaultItems = [item1, item2, item3]
 
-Item.insertMany(defaultItems, function (err) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log('Successfully saved the default items to the collection');
-    }
-})
+
 
 
 
 
 app.get("/", function (req, res) {
+
+
     Item.find({}, function (err, foundItems) {
-        res.render("list", {
-            listTitle: 'Today',
-            newListItems: foundItems
-        });
+        if (foundItems.length === 0) {
+            Item.insertMany(defaultItems, function (err) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log('Successfully saved the default items to the collection');
+                }
+            })
+            res.redirect('/')
+        } else {
+            res.render("list", {
+                listTitle: 'Today',
+                newListItems: foundItems
+            });
+        }
+
     })
 
 
