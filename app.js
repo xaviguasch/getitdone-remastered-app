66262770cda779ff3907e6dcd0b0441a1ed5d440
xaviguasch <xaviguasch@gmail.com>
2019-02-26 16:48:20 +0100

    saving an item written through the app

--- app.js
+++ app.js
@@ -71,15 +71,15 @@ app.get("/", function (req, res) {
 
 app.post("/", function (req, res) {
 
-    const item = req.body.newItem;
+    const itemName = req.body.newItem
 
-    if (req.body.list === "Work") {
-        workItems.push(item);
-        res.redirect("/work");
-    } else {
-        items.push(item);
-        res.redirect("/");
-    }
+    const item = new Item({
+        name: itemName
+    })
+
+    item.save()
+
+
 });
 
 app.get("/work", function (req, res) {
