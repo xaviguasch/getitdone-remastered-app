f7ce5d8d6f7c3d3df8def6e3c3df6778284d4e3a
xaviguasch <xaviguasch@gmail.com>
2019-02-26 12:08:58 +0100

    created new items

--- app.js
+++ app.js
@@ -24,11 +24,23 @@ const itemsSchema = new mongoose.Schema({
 
 const Item = mongoose.model('Item', itemsSchema)
 
+const item1 = new Item({
+    name: 'Welcome to your todolist!'
+})
+
+const item2 = new Item({
+    name: 'Hit the + button to add a new item'
+})
+
+const item3 = new Item({
+    name: '<-- Hit this to delete an item'
+})
+
+const defaultItems = [item1, item2, item3]
+
 
 
 app.get("/", function (req, res) {
-
-
     res.render("list", {
         listTitle: 'Today',
         newListItems: items
